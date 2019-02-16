1d4283f74f357e43759d70855a2c5fddff282b5c
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2019-02-16 13:14:20 +0700

    initial commit

--- lib/workshop_oop.rb
+++
@@ -1,6 +0,0 @@
-require "workshop_oop/version"
-
-module WorkshopOop
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/workshop_oop/version.rb
+++
@@ -1,3 +0,0 @@
-module WorkshopOop
-  VERSION = "0.1.0"
-end
